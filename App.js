a5e5e88b19e0e3878bca7b5d1ac4792aa4268ef5
penny.liu <penny.liu@kaiostech.com>
2021-04-22 15:43:04 +0800

    Learn the basics

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const Greeting = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.center}>
+      <Text>Hello {props.name}</Text>
     </View>
   );
-}
+};
 
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Greeting name='AslanT' />
+      <Greeting name='Jaina' />
+
+      <Text>You clicked {count} times</Text>
+
+      <Button
+        onPress={() => setCount(count + 1)}
+        title='Click me!'
+        color='#841584'
+        accessibilityLabel="Learn more about this purple button"
+      />
+
+      <StatusBar style='auto' />
+    </View>
+  );
+};
+
+export default App;
+
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c4d5ff99',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  center: {
+    alignItems: 'center',
+    backgroundColor: '#efefef',
   },
 });
