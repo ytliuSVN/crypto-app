17ca4168c449db6cc05a027522b8ea6012559135
penny.liu <penny.liu@kaiostech.com>
2021-04-26 10:54:05 +0800

    Implement load more FlatList with JSONPlaceholder

--- App.js
+++ App.js
@@ -1,59 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StatusBar,
   StyleSheet,
+  Image,
   Text,
   View,
   SafeAreaView,
   FlatList,
   TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 
-const DATA = [
-  {
-    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
-  },
-  {
-    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
-  },
-  {
-    id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
-  },
-];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-    <Text style={[styles.title, textColor]}>{item.title}</Text>
-  </TouchableOpacity>
-);
-
 const App = () => {
-  const [selectedId, setSelectedId] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const loadItems = async () => {
+      setIsLoading(true);
+      const apiURL = `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${page}`;
+      fetch(apiURL)
+        .then((res) => res.json())
+        .then((resJson) => {
+          setData(data.concat(resJson));
+          setIsLoading(false);
+        });
+    };
+    loadItems();
+  }, [page]);
 
   const renderItem = ({ item }) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-    const color = item.id === selectedId ? 'white' : 'black';
-
     return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{ backgroundColor }}
-        textColor={{ color }}
-      />
+      <View style={styles.itemRow}>
+        <Image style={styles.itemImage} source={{ uri: item.url }} />
+        <Text style={styles.itemText}>{item.title}</Text>
+        <Text style={styles.itemText}>{item.id}</Text>
+      </View>
     );
+  };
+
+  const renderFooter = () => {
+    return isLoading ? (
+      <View style={styles.loader}>
+        <ActivityIndicator size='large' color='#0000ff' />
+      </View>
+    ) : null;
+  };
+
+  const fetchMore = () => {
+    // increment page for the next call
+    setPage(page + 1);
+    setIsLoading(true);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
+        data={data}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        extraData={selectedId}
+        keyExtractor={(item, index) => index.toString()}
+        ListFooterComponent={renderFooter}
+        onEndReached={fetchMore}
+        onEndReachedThreshold={0.5}
       />
     </SafeAreaView>
   );
@@ -63,16 +72,25 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    backgroundColor: '#f0f6f0',
     marginTop: StatusBar.currentHeight || 0,
   },
-  item: {
-    backgroundColor: '#f0f6f0',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  itemRow: {
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+    borderBottomWidth: 1,
   },
-  title: {
-    fontSize: 32,
+  itemImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+  },
+  itemText: {
+    fontSize: 16,
+    padding: 5,
+  },
+  loader: {
+    marginTop: 10,
+    alignItems: 'center',
   },
 });
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 
 ## Useful links and resources
 - [Why I’ve built a Coinmarketcap proxy](https://medium.com/@theBliz/why-ive-built-a-coinmarketcap-proxy-e06c898b5765)
+- [Infinite scroll/Paginating FlatList with just hooks and function components](https://medium.com/@srbkrishnan/infinite-scroll-pagination-in-flatlist-with-hooks-and-function-components-c9c08bba23a8)
