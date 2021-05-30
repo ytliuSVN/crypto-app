8114c54f0995a295a1d0353f621c5c2cb86ca175
penny.liu <penny.liu@kaiostech.com>
2021-05-30 18:02:33 +0800

    :sparkles: Add Sign In and Sign Up button

--- app/navigation/SignInScreen.js
+++ app/navigation/SignInScreen.js
@@ -59,7 +59,8 @@ const SignInScreen = ({ navigation }) => {
       <View style={styles.header}>
         <Text style={styles.textHeader}>Welcome!</Text>
       </View>
-      <View style={styles.footer}>
+
+      <Animatable.View animation='fadeInUpBig' style={styles.footer}>
         <Text style={styles.textFooter}>Email</Text>
         <View style={styles.action}>
           <AntDesign name='user' color='#05375a' size={20} />
@@ -95,7 +96,50 @@ const SignInScreen = ({ navigation }) => {
             )}
           </TouchableOpacity>
         </View>
-      </View>
+
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.signIn}>
+            <LinearGradient
+              colors={['#08d4c4', '#01ab9d']}
+              style={styles.signIn}
+            >
+              <Text
+                style={[
+                  styles.textSign,
+                  {
+                    color: '#fff',
+                  },
+                ]}
+              >
+                Sign In
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUpScreen')}
+            style={[
+              styles.signIn,
+              {
+                borderColor: '#009387',
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.textSign,
+                {
+                  color: '#009387',
+                },
+              ]}
+            >
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
   );
 };
@@ -142,5 +186,20 @@ const styles = StyleSheet.create({
     marginTop: Platform.OS === 'ios' ? 0 : -4,
     paddingLeft: 10,
     color: '#05375a',
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
