af49a6348fb9c778a9fdc3f895148daa316ee32e
penny.liu <penny.liu@kaiostech.com>
2021-05-30 22:12:50 +0800

    :sparkles: Layout SignUp Screen

--- app/navigation/SignUpScreen.js
+++ app/navigation/SignUpScreen.js
@@ -1,11 +1,182 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  Platform,
+  TextInput,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { LinearGradient } from 'expo-linear-gradient';
+import { AntDesign } from '@expo/vector-icons';
+import Feather from 'react-native-vector-icons/Feather';
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
+  const initialState = {
+    email: '',
+    password: '',
+    confirm_password: '',
+    check_textInputChange: false,
+    secureTextEntry: true,
+    confirm_secureTextEntry: true,
+  };
+
+  const [data, setData] = React.useState(initialState);
+
+  const textInputChange = (val) => {
+    if (val.trim().length != 0) {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: true,
+      });
+    } else {
+      setData({
+        ...data,
+        email: val,
+        check_textInputChange: false,
+      });
+    }
+  };
+
+  const handlePasswordChange = (val) => {
+    setData({
+      ...data,
+      password: val,
+    });
+  };
+
+  const handleConfirmPasswordChange = (val) => {
+    setData({
+      ...data,
+      confirm_password: val,
+    });
+  };
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry,
+    });
+  };
+
+  const updateConfirmSecureTextEntry = () => {
+    setData({
+      ...data,
+      confirm_secureTextEntry: !data.confirm_secureTextEntry,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>SignUpScreen</Text>
-      <Button title='Click Here' onPress={() => alert('Button Clicked!')} />
+      <View style={styles.header}>
+        <Text style={styles.textHeader}>Register Now!</Text>
+      </View>
+
+      <Animatable.View animation='fadeInUpBig' style={styles.footer}>
+        <Text style={styles.textFooter}>Email</Text>
+        <View style={styles.action}>
+          <AntDesign name='user' color='#05375a' size={20} />
+          <TextInput
+            placeholder='Your Email'
+            style={styles.textInput}
+            autoCapitalize='none'
+            onChangeText={(val) => textInputChange(val)}
+          />
+
+          {data.check_textInputChange ? (
+            <Animatable.View animation='bounceIn'>
+              <Feather name='check-circle' color='green' size={20} />
+            </Animatable.View>
+          ) : null}
+        </View>
+        <Text style={[styles.textFooter, { marginTop: 35 }]}>Password</Text>
+        <View style={styles.action}>
+          <AntDesign name='lock1' color='#05375a' size={20} />
+          <TextInput
+            placeholder='Your Password'
+            secureTextEntry={data.secureTextEntry ? true : false}
+            style={styles.textInput}
+            autoCapitalize='none'
+            onChangeText={(val) => handlePasswordChange(val)}
+          />
+
+          <TouchableOpacity onPress={updateSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name='eye-off' color='green' size={20} />
+            ) : (
+              <Feather name='eye' color='grey' size={20} />
+            )}
+          </TouchableOpacity>
+        </View>
+        <Text style={[styles.textFooter, { marginTop: 35 }]}>
+          Confirm Password
+        </Text>
+        <View style={styles.action}>
+          <AntDesign name='lock1' color='#05375a' size={20} />
+          <TextInput
+            placeholder='Confirm Your Password'
+            secureTextEntry={data.confirm_secureTextEntry ? true : false}
+            style={styles.textInput}
+            autoCapitalize='none'
+            onChangeText={(val) => handleConfirmPasswordChange(val)}
+          />
+
+          <TouchableOpacity onPress={updateConfirmSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name='eye-off' color='green' size={20} />
+            ) : (
+              <Feather name='eye' color='grey' size={20} />
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.signIn}>
+            <LinearGradient
+              colors={['#08d4c4', '#01ab9d']}
+              style={styles.signIn}
+            >
+              <Text
+                style={[
+                  styles.textSign,
+                  {
+                    color: '#fff',
+                  },
+                ]}
+              >
+                Sign Up
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={[
+              styles.signIn,
+              {
+                borderColor: '#009387',
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.textSign,
+                {
+                  color: '#009387',
+                },
+              ]}
+            >
+              Sign In
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
   );
 };
@@ -15,7 +186,57 @@ export default SignUpScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#03AE9D',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  textHeader: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  textFooter: {
+    color: '#05375a',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -4,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  button: {
     alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
